234c0384ae607edf0b1e82bff76b423939b812b1
geoffreyfulton <gfnx410@gmail.com>
2019-09-22 05:07:50 -0400

    integrated wtf-forms and added session handling logic to the index route

--- hello.py
+++ hello.py
@@ -1,7 +1,8 @@
 from datetime import datetime
 
-from flask import Flask, request, render_template
+from flask import Flask, request, render_template, session, redirect, url_for
 app = Flask(__name__)
+app.config['SECRET_KEY'] = 'replace with secure key'
 
 from flask_bootstrap import Bootstrap
 bootstrap = Bootstrap(app)
@@ -9,14 +10,29 @@ bootstrap = Bootstrap(app)
 from flask_moment import Moment
 moment = Moment(app)
 
+from flask_wtf import FlaskForm
+from wtforms import StringField, SubmitField
+from wtforms.validators import DataRequired
 
-@app.route('/')
+
+class NameForm(FlaskForm):
+    name = StringField('What is your name?', validators=[DataRequired()])
+    submit = SubmitField('Submit')
+
+
+@app.route('/', methods=['GET', 'POST'])
 def index():
-    return render_template('index.html', current_time=datetime.utcnow())
+    form = NameForm()
+    if form.validate_on_submit():
+        session['name'] = form.name.data
+        return redirect(url_for('index'))
+    return render_template('index.html', form=form, name=session.get('name'), current_time=datetime.utcnow())
+
 
 @app.route('/about')
 def about():
     return '<h1>About</h1>'
+
 
 @app.route('/client-info')
 def client_info():
